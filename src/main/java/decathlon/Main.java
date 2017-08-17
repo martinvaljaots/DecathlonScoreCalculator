de447380020abf3e47b30f0314dcf5b63c419959
Martin V <martin.valjaots@gmail.com>
2017-08-17 19:01:14 +0300

    Added sorting and printing to main. Next need to finally put tests in separate package.

--- src/main/java/decathlon/Main.java
+++ src/main/java/decathlon/Main.java
@@ -1,8 +1,7 @@
 package main.java.decathlon;
 
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
@@ -10,13 +9,31 @@ public class Main {
 	    // runs InfoReader.loadFile for the time being
         InfoReader ir = new InfoReader();
         List<AthleteInfo> athleteInfoList = new ArrayList<>();
+
         try {
-            athleteInfoList = Sort.bogoSort(Calculations.calculateTotalPoints(ir.loadFile()));
+            athleteInfoList = ir.loadFile();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        for(AthleteInfo ai: athleteInfoList) {
-            System.out.println(ai.getTotalPoints());
+        athleteInfoList = Calculations.calculateTotalPoints(athleteInfoList);
+
+        Collections.sort(athleteInfoList, new Comparator<AthleteInfo>() {
+
+            @Override
+            public int compare(AthleteInfo athlete1, AthleteInfo athlete2) {
+                if (athlete1.getTotalPoints() < athlete2.getTotalPoints()) {
+                    return 1;
+                }
+                if (athlete1.getTotalPoints() > athlete2.getTotalPoints()) {
+                    return -1;
+                }
+                return 0;
+            }
+        });
+        int i = 0;
+        for(AthleteInfo ai : athleteInfoList) {
+            System.out.println(i + ". " + ai.getName() + " (" + ai.getTotalPoints() + ")");
+            i++;
         }
     }
 }
